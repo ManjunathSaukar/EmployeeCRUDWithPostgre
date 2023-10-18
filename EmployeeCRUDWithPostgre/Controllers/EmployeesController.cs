0269227f063db50ff6552568b9389cf60a028bd9
Saukar Manjunath <saukar@monocept.com>
2023-10-18 13:55:54 +0530

    Add project files.

---
+++ EmployeeCRUDWithPostgre/Controllers/EmployeesController.cs
@@ -0,0 +1,64 @@
+﻿using EmployeeCRUDWithPostgre.Models;
+using EmployeeCRUDWithPostgre.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeCRUDWithPostgre.Controllers
+{
+    [Route("api/employees")] // Changed the base route to "api/employees"
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeesController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet("all")] // Renamed the endpoint to "api/employees/all"
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var result = await _employeeService.GetEmployeeList();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")] // This is now "api/employees/{id}"
+        public async Task<IActionResult> GetEmployeeById(int id)
+        {
+            var result = await _employeeService.GetEmployee(id);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost("add")] // Renamed the endpoint to "api/employees/add"
+        public async Task<IActionResult> AddEmployee([FromBody] Employee employee)
+        {
+            var result = await _employeeService.CreateEmployee(employee);
+
+            return Ok(result);
+        }
+
+        [HttpPut("update")] // Renamed the endpoint to "api/employees/update"
+        public async Task<IActionResult> UpdateEmployee([FromBody] Employee employee)
+        {
+            var result = await _employeeService.UpdateEmployee(employee);
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")] // This is now "api/employees/{id}"
+        public async Task<IActionResult> DeleteEmployeeById(int id)
+        {
+            var result = await _employeeService.DeleteEmployee(id);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ EmployeeCRUDWithPostgre/Program.cs
@@ -0,0 +1,41 @@
+using Dapper;
+using EmployeeCRUDWithPostgre.Services;
+
+namespace EmployeeCRUDWithPostgre
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DefaultTypeMap.MatchNamesWithUnderscores = true;
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddScoped<IDbService, DbService>();
+            builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
